05c4dbdf1d8fc93490402845acab12219dd07f52
Johannes Merwe <johannes@leadinfo.com>
2024-05-04 10:47:01 +0200

    if it works,commit

--- src/components/Form/SpeedOfReact.jsx
+++ src/components/Form/SpeedOfReact.jsx
@@ -1,20 +1,30 @@
 import {useState} from 'react';
 
 export default function MovingDot() {
+    const [delay, setDelay] = useState(0);
     const [position, setPosition] = useState({
         x: 0,
         y: 0
     });
+
+    let start = 0;
+    let end = 0;
+
     return (
         <>
             <h2>X: {position.x}</h2>
             <h2>Y: {position.y}</h2>
+            <h1>Delay: {delay} ms</h1>
             <div
                 onPointerMove={e => {
+                    start = Date.now();
+                    end = 0;
                     setPosition({
                         x: e.clientX,
                         y: e.clientY
                     });
+                    end =  Date.now() - start;
+                    setDelay(end);
                 }}
                 style={{
                     position: 'relative',
